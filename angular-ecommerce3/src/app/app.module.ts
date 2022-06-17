0ced218f43ad87f0a7ace5dd7c1d63bce8996d88
Genbuos <swiftypython18@gmail.com>
2022-06-17 18:59:10 -0400

    updates

--- angular-ecommerce3/src/app/app.module.ts
+++ angular-ecommerce3/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import{NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule} from '@angular/common/http';
@@ -31,6 +32,7 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
+    NgbModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
