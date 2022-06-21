834bc934d6c31259a7715893e3e114c9aacc4fac
Genbuos <swiftypython18@gmail.com>
2022-06-21 01:09:51 -0400

    updates

--- angular-ecommerce3/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce3/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -17,8 +18,37 @@ export class CheckoutComponent implements OnInit {
         firstName:[''],
         lastName: [''],
         email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard:this.formBuilder.group({
+        cardtype: [''],
+        nameOnCard: [''],
+          cardNumber: [''],
+          securityCode: [''],
+          expirationMonth: [''],
+          expirationYear: ['']
       })
     });
   }
+onSubmit(){
+  console.log("Handling the submit button")
+  
+  console.log(this.checkoutFormGroup.get('customer').value);
+  console.log(this.checkoutFormGroup.get('customer').value.email);
+}
 
 }
